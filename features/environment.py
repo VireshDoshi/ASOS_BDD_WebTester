9c22448aef22566d3a0fb98691d6738d92c6fea7
Thomas-Rice <tikkleanim@gmail.com>
2017-05-10 23:17:04 +0100

    second purple shirt test

--- features/environment.py
+++ features/environment.py
@@ -1,16 +1,27 @@
 from seleniumWrapper import SeleniumWrapper
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 
 
 
 def before_all(context):
   driver = webdriver.Firefox()
-  driver.implicitly_wait(10)
   context.driver = driver
+  context.driver.implicitly_wait(10)
   context.keys = Keys
+  context.WebDriverWait = WebDriverWait
+  context.EC = EC
+  context.TimeoutException = TimeoutException
+  context.ActionChains = ActionChains
 
 
-def after_all(context):
-  # context.driver.quit()
-    pass
+# def after_all(context):
+#   context.driver.quit()
+# purple = context.driver.find_element_by_xpath('//div[@class="facetvalue-name" and text()="Purple"]')
+# //div[8]/div/div/ul/li[13]/a/span[2]
--- features/steps/find_shirts.py
+++ features/steps/find_shirts.py
@@ -4,25 +4,33 @@ from behave import *
 @given('I want to order a shirt')
 def step_impl(context):
     context.driver.get("http://www.asos.com")
-    context.implicitly_wait(10)
     assert "ASOS" in context.driver.title
 
 
 
 @when('I search for purple t shirts')
 def step_impl(context):
-    searchBox = context.driver.find_element_by_class_name("search-box")
-    print(searchBox.text)
-    searchBox.clear()
-    searchBox.send_keys("purple shirt")
-    searchBox.send_keys(context.keys.RETURN)
+    # searchBox = context.driver.find_element_by_class_name("search-box")
+    # searchBox.clear()
+    # searchBox.send_keys("purple shirt")
+    # searchBox.send_keys(context.keys.RETURN)
+    test = context.driver.find_element_by_xpath('//a[@href="http://www.asos.com/men/shirts/cat/?cid=3602"]')
+    men = context.driver.find_element_by_xpath('//a[@href="http://www.asos.com/men/"]')
+    actions = context.ActionChains(context.driver)
+    actions.move_to_element(men)
+    actions.click(test)
+    actions.perform()
+    purple = context.driver.find_element_by_xpath('//div[8]/div/div/ul/li[13]/a/span[1]')
+    purple.click()
+    print(purple.is_selected())
 
-
+#not convinced this is the best way of confirming the new page but it's a start
+#TODO - think of a better method
 @then('I should see some purple t shirts')
 def step_impl(context):
-    # import time
-    # time.sleep(2)
-    print (context.driver.current_url)
-    assert context.driver.current_url == 'http://www.asos.com/search/purple-shirt?q=purple+shirt'
-    pass
-    # context.driver.quit()
+    try:
+        context.WebDriverWait(context.driver, 3).until(
+            context.EC.title_contains("purple shirt"))
+    except context.TimeoutException:
+        raise Exception("Loading took too much time!")
+
