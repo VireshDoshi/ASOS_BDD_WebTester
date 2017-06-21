296da87d9181e71524747ee1dd2aa2dda8f3b550
Thomas-Rice <tikkleanim@gmail.com>
2017-06-21 17:04:59 +0100

    Delete Find_Purple_Shirts_SearchBar.py

--- features/steps/Find_Purple_Shirts_SearchBar.py
+++
@@ -1,7 +0,0 @@
-from behave import *
-
-@when('I search for purple t shirts via the searchbar')
-def step_impl(context):
-    #Click on search box and send 'purple shirts' with a press of return.
-    context.basePageObject.set_text('Class', "search-box", "purple shirt")
-
